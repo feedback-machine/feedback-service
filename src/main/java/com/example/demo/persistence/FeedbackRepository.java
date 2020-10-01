4564b8a8619b712e061749557504104852fa9849
vrokida <vrokida@gmail.com>
2020-10-01 13:20:59 -0500

    [Vero]: Removing mongo dependency

--- src/main/java/com/example/demo/persistence/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.persistence;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface FeedbackRepository extends MongoRepository<Feedback, String> {
-
-
-}
